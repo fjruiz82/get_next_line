db23ba1bb878165dce97cd3cdd95d77cc54cbace
Francisco javier Ruiz carvajal <fruiz-ca@c1r11s4.42malaga.com>
2022-05-19 12:42:09 +0200

    segundo

--- get_next_line.c
+++ get_next_line.c
@@ -6,19 +6,31 @@
 /*   By: fruiz-ca <fruiz-ca@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/05/19 07:15:24 by fruiz-ca          #+#    #+#             */
-/*   Updated: 2022/05/19 10:19:43 by fruiz-ca         ###   ########.fr       */
+/*   Updated: 2022/05/19 12:08:50 by fruiz-ca         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "get_next_line.h"
 
+
+
 char	*get_next_line(int fd)
 {
     char *buf;
     size_t nbytes;
-    size_t bytes_read;
+    size_t BUFFER_SIZE;
+    int text_len;
 
-    nbytes = sizeof(buf);
-    bytes_read = read (fd, buf, nbytes);
+
+    buf = malloc(sizeof * buf * (BUFFER_SIZE + 1));
+    if (!buf)
+        return (NULL);
+
+    text_len = read (fd, buf, BUFFER_SIZE);
+    while (text_len != 0)
+    {
+        
+    }
+
     
 }
